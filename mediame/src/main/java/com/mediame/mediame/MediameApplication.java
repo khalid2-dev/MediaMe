ca9bed4de3f0211bb1c691b2ca3ee466d8b403af
khalid2-dev <k.abushaban2@gmail.com>
2024-07-15 12:22:21 -0400

    Modifying Project

--- mediame/src/main/java/com/mediame/mediame/MediameApplication.java
+++ mediame/src/main/java/com/mediame/mediame/MediameApplication.java
@@ -2,8 +2,9 @@ package com.mediame.mediame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MediameApplication {
 
 	public static void main(String[] args) {
